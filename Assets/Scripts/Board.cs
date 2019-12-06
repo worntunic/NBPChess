7b66c3822a9234ed15b6772485539c2492631380
worntunic <lasernatunika@gmail.com>
2019-12-06 19:07:05 +0100

    Board created, tiles created.
    Board and tile generation.
    Detection of hovering and selection.
    Effects for hover and select.

---
+++ Assets/Scripts/Board.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace NBPChess
+{
+	public class Board : MonoBehaviour
+	{
+		public Tile[,] board;
+		public Tile model;
+
+		public void Awake()
+		{
+			GenerateBoard();
+		}
+
+		public void GenerateBoard()
+		{
+			board = new Tile[8,8];
+			for(int i = 0; i < 8; i++)
+			{
+				for(int j = 0; j < 8; j++)
+			{
+
+					board[i,j] = Instantiate(model, transform);
+					board[i,j].Initialize(i, j);
+				}
+			}
+		}
+	}
+}
+
